be8eccb206307f84bd76ba1989d1a836448aa6a9
detule <ogjoneski@gmail.com>
2014-12-18 15:14:01 -0500

    rflot: Bugfix: On plot update for now just re-create plot.
    Implement shim for update plot code-path.

--- inst/htmlwidgets/rflot.js
+++ inst/htmlwidgets/rflot.js
@@ -14,46 +14,53 @@ HTMLWidgets.widget({
   },
 
   renderValue: function(el, x, instance) {
-
     var self = this;
+    if (instance.flot) {
+      self.updatePlot(self, el, x, instance);
+    } else {  // create new instance
+      self.createPlot(el, x, instance);
+    }
+  },
 
+  createPlot: function(el, x, instance) {
     var series = x.series;
     var options = x.options;
 
-    if (instance.flot) {
-      // update exisigng instance
-      // Not yet implemented
-    } else {  // create new instance
-
-      var plot = $.plot(el,series,options);
-      if(typeof(options.onClick) != 'undefined') {
-        $(el).on("plotclick", options.onClick);
-      }
-
-      $(el).on("plotselected", function (event, ranges) {
-        $.each(plot.getAxes(), function (name, axis) {
-          if (axis.used && typeof(ranges[name]) != 'undefined') {
-            axis.options.min = ranges[name].from;
-            axis.options.max = ranges[name].to;
-          }
-        });
-        plot.setupGrid();
-        plot.draw();
-        plot.clearSelection();
-      });
-
-      //Can we move this to event 'plotunselected'
-      $(el).on("dblclick", function () {
-        $.each(plot.getAxes(), function(_, axis) {
-          var opts = axis.options;
-          opts.min = null;
-          opts.max = null;
-        });
-        plot.setupGrid();
-        plot.draw();
-        plot.clearSelection();
-      });
-      instance.flot = plot;
+    var plot = $.plot(el,series,options);
+    if(typeof(options.onClick) != 'undefined') {
+      $(el).on("plotclick", options.onClick);
     }
+
+    $(el).on("plotselected", function (event, ranges) {
+      $.each(plot.getAxes(), function (name, axis) {
+        if (axis.used && typeof(ranges[name]) != 'undefined') {
+          axis.options.min = ranges[name].from;
+          axis.options.max = ranges[name].to;
+        }
+      });
+      plot.setupGrid();
+      plot.draw();
+      plot.clearSelection();
+    });
+
+    //Can we move this to event 'plotunselected'
+    $(el).on("dblclick", function () {
+      $.each(plot.getAxes(), function(_, axis) {
+        var opts = axis.options;
+        opts.min = null;
+        opts.max = null;
+      });
+      plot.setupGrid();
+      plot.draw();
+      plot.clearSelection();
+    });
+    instance.flot = plot;
   },
+
+  updatePlot: function(self, el, x, instance) {
+    // update exisigng instance
+    // Not yet implemented
+    self.createPlot(el, x, instance);
+  }
+
 });
